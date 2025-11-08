8e6e044a820481cc7ed61016f49d8d3e35b572ca
王丛 <13577756+wang-cong00111@user.noreply.gitee.com>
2025-11-08 08:37:03 +0800

    重构完成：统一表单组件、主题获取方式，完善项目交接文档

--- har/ui-dialog/oh_modules/@wuba58/omni-ui/src/main/ets/components/richtext/utils/ExposeData.ts
+++
@@ -1,9 +0,0 @@
-
-export interface ExposeData {
-  // 字段别名
-  name?: string
-  // 原始字段名
-  propertyName?: string
-  // 转换执行回调函数
-  transform?: Function
-}
--- har/ui-dialog/oh_modules/@wuba58/omni-ui/src/main/ets/components/richtext/utils/index.ts
+++
@@ -1,7 +0,0 @@
-import { MetadataStorage } from './MetadataStorage';
-
-export type ObjectLike = Record<PropertyKey, any>
-export const defaultMetadataStorage = new MetadataStorage();
-export declare type ClassConstructor<T> = {
-  new (...args: any[]): T;
-};
